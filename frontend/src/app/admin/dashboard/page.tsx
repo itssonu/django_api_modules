6c06826493f651dc469ce573e60dbdcdba90299b
sonutechbit <sonu.techbit@outlook.com>
2025-01-17 16:20:23 +0530

    logout added

--- frontend/src/app/admin/dashboard/page.tsx
+++ frontend/src/app/admin/dashboard/page.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import ProtectedRoute from '@/components/ProtectedRoute'
+import { useAuth } from '@/contexts/AuthContext'
 import React from 'react'
 
 const Dashboard = () => {
+    const { logout } = useAuth()
+
     return (
         <ProtectedRoute>
             <div>Dashboard</div>
+            <button type='button' onClick={logout}>logout</button>
         </ProtectedRoute>
     )
 }
